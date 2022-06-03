4380f5875179672399d863cd9968dea58497c510
yusrilalbi <aa684011@gmail.com>
2022-06-04 05:28:47 +0700

    Front End 70% (*Try Commit From AS Directly)

--- Andro/FV_Classification/app/src/main/java/com/example/fv_classification/goal.kt
+++ Andro/FV_Classification/app/src/main/java/com/example/fv_classification/goal.kt
@@ -7,5 +7,7 @@ class goal : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_goal)
+
+
     }
 }
